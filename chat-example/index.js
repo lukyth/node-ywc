dca5288578fc866db5c6a34291ef6b5f214054ba
chenka <sarunyhot@gmail.com>
2016-03-13 00:44:34 +0700

    Add example chat app

---
+++ chat-example/index.js
@@ -0,0 +1,16 @@
+'use strict'
+let express = require('express')
+let app = express()
+let http = require('http').Server(app)
+let io = require('socket.io')(http)
+let path = require('path')
+const PORT = 3001
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+http.listen(PORT, () =>{
+  console.log(`listening on *:${PORT}`)
+})
